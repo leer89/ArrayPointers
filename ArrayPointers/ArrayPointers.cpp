3877efafa81193a8ca4229b3d6665bb5e5e1b03c
leer89 <leer89@gmail.com>
2016-11-23 16:21:19 -0500

    base project

--- ArrayPointers/ArrayPointers.cpp
+++ ArrayPointers/ArrayPointers.cpp
@@ -3,9 +3,31 @@
 
 #include "stdafx.h"
 
+typedef int* IntPtr; // lets "IntPtr" define variables as an type integer pointer
 
 int main()
 {
-    return 0;
-}
+	using namespace std;
 
+	IntPtr p;
+	int a[10]; // declare array of [10] 
+	int index; // declare integer variable "index"
+
+	for (index = 0; index < 10; index++)
+		a[index] = index;
+
+	p = a; // set 'a' to pointer 'p'
+
+	for (index = 0; index < 10; index++)
+		cout << p[index] << " ";
+	cout << endl;
+
+	for (index = 0; index < 10; index++)
+		p[index] = p[index] + 1;
+
+	for (index = 0; index < 10; index++)
+		cout << a[index] << " ";
+	cout << endl;
+
+	return 0;
+}
